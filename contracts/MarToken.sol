372851c65e70519c587896c123cebb5864fd1908
Mchain <marbellachain@gmail.com>
2023-09-23 09:13:09 +0200

    function bulkTransfer

--- contracts/MarToken.sol
+++ contracts/MarToken.sol
@@ -11,6 +11,11 @@ contract MarToken is ERC20, Ownable, ERC20Permit {
     uint8 private _decimals;
 
     event Burn(address indexed from, uint256 value);
+    event BulkTransferCompleted(
+        address indexed token,
+        address indexed sender,
+        uint256 totalAmount
+    );
     event TokensRecovered(
         address indexed tokenAddress,
         address indexed recipient,
@@ -56,6 +61,59 @@ contract MarToken is ERC20, Ownable, ERC20Permit {
         emit Burn(msg.sender, amount);
 
         return true;
+    }
+
+    /**
+     * @dev Transfers tokens in bulk by aggregating them in the contract first.
+     * Ensures atomicity of the bulk transfer.
+     *
+     * Requirements:
+     *
+     * - The number of recipients must be the same as the number of amounts.
+     * - The caller must have a balance that is greater or equal to the sum of all amounts.
+     * - Each recipient address must be non-zero.
+     *
+     * Emits a {BulkTransferCompleted} event upon successful transfer.
+     *
+     * @param recipients List of addresses to which the tokens will be transferred.
+     * @param amounts List of amounts to transfer to respective recipients.
+     */
+    function bulkTransfer(
+        address[] memory recipients,
+        uint256[] memory amounts
+    ) public {
+        require(
+            recipients.length == amounts.length,
+            "The number of recipients should be equal to the number of amounts"
+        );
+        IERC20 token = IERC20(address(this));
+
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < amounts.length; i++) {
+            totalAmount += amounts[i];
+        }
+
+        require(
+            token.balanceOf(msg.sender) >= totalAmount,
+            "Insufficient balance for bulk transfer"
+        );
+
+        require(
+            token.allowance(msg.sender, address(this)) >= totalAmount,
+            "Contract not approved for the total transfer amount"
+        );
+
+        token.safeTransferFrom(msg.sender, address(this), totalAmount);
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            require(
+                recipients[i] != address(0),
+                "Recipient address cannot be zero"
+            );
+            token.safeTransferFrom(address(this), recipients[i], amounts[i]);
+        }
+
+        emit BulkTransferCompleted(address(token), msg.sender, totalAmount);
     }
 
     // Receives ether sent to the contract.
